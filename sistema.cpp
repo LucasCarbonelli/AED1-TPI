dd74bbd28a23126f0ba67c454bc2a7922eaf5b62
istng <ivanstng@gmail.com>
2016-05-22 20:02:02 -0300

    estuve laburando con sistema.cpp, y mando tambien lo hecho de campo que dije que hacia

--- sistema.cpp
+++ sistema.cpp
@@ -30,14 +30,93 @@ void Sistema::crecer()
 
 void Sistema::seVinoLaMaleza(const Secuencia<Posicion>& ps)
 {
+	//aca no hablo de this? igual me pasan como const la secuencia ps, no puedo hacer esto?? otra: me armo un PS que tenga todos los p de ps, sacados de this,
+	//y a eso le hago esto. Hice algo parecido a esto en el ejer despegar, mas abajo.
+	int i = 0;
+	while (i < ps.size()) {
+		estadoDelCultivo(p[i]) = ConMaleza;
+		i++;
+	}
 }
 
 void Sistema::seExpandePlaga()
 {
+	int i = 0;
+	Secuencia<Posicion> PosConPlaga;
+
+	//quiza ese lo paso a auxiliar
+	while (i < this.campo().dimensiones().ancho) {
+
+		while (j < this.campo().dimensiones().largo) {
+			Posicion p;
+			p.x = i;
+			p.y = j;
+			//se puede preguntar y o asignar estado de cultivo a posiciones así? funciona así esto? no hay que hacer pasaje por stringtoalgo ni nada?
+			if (this.estadoDelCultivo(p) == ConPlaga)
+			{
+				PosConPlaga.push_back(p);
+			}
+			j++
+		}
+		i++
+	}
+
+	//quiza esto lo paso a auxiliar
+	int k = 0;
+	while (k < PosConPlaga.size()) {
+		Posicion p;
+		p.x = PosConPlaga[k].x
+		p.y = PosConPlaga[k].y
+		//al final x es el largo o el ancho??
+		if (PosConPlaga[k].x + 1 <= this.campo().dimensiones().largo)
+		{
+			p.x = PosConPlaga[k].x + 1;
+			this.estadoDelCultivo(p) = ConPlaga;
+		}
+		//nose si es necesario aclarar que en este caso esa pos debe ser menor que el largo, ya que si la pos original era menor al largo, -1 lo va a ser seguro.
+		if (PosConPlaga[k].x - 1 >= 0 && PosConPlaga[k].x - 1 <= this.campo().dimensiones().largo)
+		{
+			p.x = PosConPlaga[k].x - 1;
+			this.estadoDelCultivo(p) = ConPlaga;
+		}
+		if (PosConPlaga[k].y + 1 <= this.campo().dimensiones().ancho)
+		{
+			p.x = PosConPlaga[k].x;
+			p.y = PosConPlaga[k].y + 1;
+			this.estadoDelCultivo(p) = ConPlaga;
+		}
+		//idem arriba.
+		if (PosConPlaga[k].y - 1 >= 0 && PosConPlaga[k].y - 1 <= this.campo().dimensiones().ancho)
+		{
+			p.x = PosConPlaga[k].x;
+			p.y = PosConPlaga[k].y - 1;
+			this.estadoDelCultivo(p) = ConConPlaga;
+		}
+	}
 }
 
 void Sistema::despegar(const Drone & d)
 {
+	//mismo problema que en seVinoLaMaleza con el tema de parametro con const
+	if (buscarDrone(d, this))
+	{
+		D = d; //como asignador, se puede? sino hago un aux, o pongo todo lo que signfica acá.
+		Posicion p;
+		p.x = 1;
+		p.y = 1;
+		if(posicionLibre(p, this)) {
+			D.enVuelo() && D.bateria() = 100;
+		}
+		if (posicionLibre(posG, this) && (posG.x != 1 || posG.y !=1))
+		{
+			D.enVuelo() && D.bateria() = 100;
+		}
+	}
+
+
+	//lo de posG lo puse porque lo pasan por parametro en crear campo, en el cpp... medio turbio igual, sino como se donde esta el granero?
+	//puse dos if por el caso del campo creado sin parametros
+	
 }
 
 bool Sistema::listoParaCosechar() const
